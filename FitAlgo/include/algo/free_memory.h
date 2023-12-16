a9b57b5fdf4558f915f76e8447b5ba70e98480cc
GabroveanuRazvan <gabroveanu@gmail.com>
2023-12-16 16:21:19 +0200

    Added header and .c file for freeMem method.

---
+++ FitAlgo/include/algo/free_memory.h
@@ -0,0 +1,6 @@
+#ifndef _MA_FREE_MEMORY_H
+#define _MA_FREE_MEMORY_H
+
+
+
+#endif
---
+++ FitAlgo/src/free_memory.c
@@ -0,0 +1,5 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../include/block.h"
